f28ab81d1481c632cb2cf50e682ef1e7f425368e
Peter Babo <pbabo17@student.aau.dk>
2019-03-30 18:59:03 +0100

    JUCE MIDI input example
    Added JUCE MIDI input tutorial file, that is modifeid so it can read any external MIDI input.

--- LaserHarpMIDI/LaserHarpMIDI.ino
+++ LaserHarpMIDI/LaserHarpMIDI.ino
@@ -107,6 +107,7 @@ void potentiometers() {
   for (int i = 0; i < NPots; i++) { // Loops through all the potentiometers
 
     potCState[i] = analogRead(potPin[i]); // Reads the pot and stores it in the potCState variable
+    /*
     if (potCState[i] > 900) {
       if (!test) {
         MIDI.sendNoteOn(note + i, 127, midiCh + 1); // note, velocity, channel
@@ -117,8 +118,9 @@ void potentiometers() {
       MIDI.sendNoteOn(note + i, 0, midiCh + 1);
       test = false;
     }
+    */
 
-    midiCState[i] = map(potCState[i], 0, 500, 127, 0); // Maps the reading of the potCState to a value usable in midi
+    midiCState[i] = map(potCState[i], 0, 400, 127, 0); // Maps the reading of the potCState to a value usable in midi
 
 
     potVar = abs(potCState[i] - potPState[i]); // Calculates the absolute value between the difference between the current and previous state of the pot
@@ -141,7 +143,7 @@ void potentiometers() {
 
         // use if using with ATmega328 (uno, mega, nano...)
         //do usbMIDI.sendControlChange if using with Teensy
-        //MIDI.sendControlChange(cc + i, midiCState[i], midiCh); // cc number, cc value, midi channel
+        MIDI.sendControlChange(cc + i, midiCState[i], midiCh); // cc number, cc value, midi channel
 
         // use if using with ATmega32U4 (micro, pro micro, leonardo...)
         //        controlChange(midiCh, cc + i, midiCState[i]); //  (channel, CC number,  CC value)
