8cd414031b00dd8f3da6511b109ce87fd7f4c792
Peter Babo <pbabo17@student.aau.dk>
2019-08-22 12:50:39 +0200

    Custom MIDI-IN
    Added custom MIDI-IN modules to support CC

--- LaserHarpMIDI/LaserHarpMIDI.ino
+++ LaserHarpMIDI/LaserHarpMIDI.ino
@@ -12,8 +12,13 @@ bool block1 = false; //Used the check if a beam is blocked
 bool block2 = false;
 bool block3 = false;
 bool block4 = false;
+
+bool unBlock1 = false; //Used the check if a beam is blocked
+bool unBlock2 = false;
+bool unBlock3 = false;
+bool unBlock4 = false;
 byte midiCh = 1; // MIDI channel to be used
-int threshold = 400;
+int threshold = 300;
 
 void setup() {
 
@@ -39,14 +44,16 @@ void potentiometers() {
   //Serial.println(potCState[0]);
   if(potCState[0] > threshold) {
     if(!block1) {
-      MIDI.sendNoteOn(40, 127, midiCh);
+      MIDI.sendNoteOn(60, 127, midiCh);
       block1 = true;
+      unBlock1 = false;
     } else {
-      midiCState[0] = map(potCState[0], threshold, 1000, 127, 0); // Maps the LDR value to be used in MIDI
+      midiCState[0] = map(potCState[0], threshold, 800, 127, 0); // Maps the LDR value to be used in MIDI
       MIDI.sendControlChange(1, midiCState[0], midiCh); // controller number, controller value, MIDI channel
     }
-  } else {
-    MIDI.sendNoteOn(40, 0, midiCh);
+  } else if (!unBlock1){
+    MIDI.sendNoteOn(60, 0, midiCh);
+    unBlock1 = true;
     block1 = false;
   }
   
@@ -54,14 +61,16 @@ void potentiometers() {
   //Serial.println(potCState[1]);
   if(potCState[1] > threshold) {
     if(!block2) {
-      MIDI.sendNoteOn(50, 127, midiCh);
+      MIDI.sendNoteOn(70, 127, midiCh);
+      unBlock2 = false;
       block2 = true;
     } else {
       midiCState[1] = map(potCState[1], threshold, 1000, 127, 0); // Maps the LDR value to be used in MIDI
       MIDI.sendControlChange(1, midiCState[1], midiCh); // controller number, controller value, MIDI channel
     }
-  } else {
-    MIDI.sendNoteOn(50, 0, midiCh);
+  } else if (!unBlock2) {
+    MIDI.sendNoteOn(70, 0, midiCh);
+    unBlock2 = true;
     block2 = false;
   }
 
@@ -69,14 +78,16 @@ void potentiometers() {
   //Serial.println(potCState[2]);
   if(potCState[2] > threshold) {
     if(!block3) {
-      MIDI.sendNoteOn(60, 127, midiCh);
+      MIDI.sendNoteOn(80, 127, midiCh);
+      unBlock3 = false;
       block3 = true;
     } else {
       midiCState[2] = map(potCState[2], threshold, 1000, 127, 0); // Maps the LDR value to be used in MIDI
       MIDI.sendControlChange(1, midiCState[2], midiCh); // controller number, controller value, MIDI channel
     }
-  } else {
-    MIDI.sendNoteOn(60, 0, midiCh);
+  } else if (!unBlock3) {
+    MIDI.sendNoteOn(80, 0, midiCh);
+    unBlock3 = true;
     block3 = false;
   }
 
@@ -84,14 +95,16 @@ void potentiometers() {
   //Serial.println(potCState[3]);
   if(potCState[3] > threshold) { 
     if(!block4) {
-      MIDI.sendNoteOn(70, 127, midiCh);
+      MIDI.sendNoteOn(90, 127, midiCh);
+      unBlock4 = false;
       block4 = true;
     } else {
       midiCState[3] = map(potCState[3], threshold, 1000, 127, 0); // Maps the LDR value to be used in MIDI
       MIDI.sendControlChange(1, midiCState[3], midiCh); // controller number, controller value, MIDI channel
     }
-  } else {
-    MIDI.sendNoteOn(70, 0, midiCh);
+  } else if (!unBlock4) { 
+    MIDI.sendNoteOn(90, 0, midiCh);
+    unBlock4 = true;
     block4 = false;
   }
 }
